ade0d8d6356a8f793d05cc639bc80b7d3876e236
Justin Eltoft <jdeltoft@gmail.com>
2015-01-13 20:36:30 -0600

    fixed bind
    fixed up binding and configure but finger printing still doesn't work.

--- devicetype.nxp.jn5168-001-M00/devicetype.nxp.jn5168-001-M00.groovy
+++ devicetype.nxp.jn5168-001-M00/devicetype.nxp.jn5168-001-M00.groovy
@@ -5,8 +5,11 @@ metadata {
        	capability "Refresh"
 		capability "Configuration"
 
-		fingerprint profileId: "0104", deviceId:"0x0107", inClusters: "0000,0003,0406", outClusters: "0003"
+		fingerprint inClusters: "0000,0003,0406", outClusters: "0003,0004,0005", manufacturer: "NXP", model: "ZHA-OccupancySensor"
+		fingerprint inClusters: "0000,0003,0406", outClusters: "0003,0004,0005", manufacturer: "NXP"
 	}
+
+//fingerprint profileId: "0104", deviceId:"0x0107", inClusters: "0000,0003,0406", outClusters: "0003,0004,0005", manufacturer: "NXP"
 
 	// simulator metadata
 	simulator {
@@ -35,23 +38,68 @@ metadata {
 
 // Parse incoming device messages to generate events
 def parse(String description) {
-   log.debug "JDE: description: $description"
-   return []
+    log.debug "JDE: description: $description"
+    
+   	Map map = [:]
+	if (description?.startsWith('read attr -')) {
+		map = parseReportAttributeMessage(description)
+	}
+ 
+	log.debug "Parse returned $map"
+    
+	def result = map ? createEvent(map) : null
+    
+    return result
+
+}
+
+//
+//JDE: description: read attr - raw: 74510104060800001800, dni: 7451, endpoint: 01, cluster: 0406, size: 08, attrId: 0000, encoding: 18, value: 00
+//
+private Map parseReportAttributeMessage(String description) {
+	Map descMap = (description - "read attr - ").split(",").inject([:]) { map, param ->
+		def nameAndValue = param.split(":")
+		map += [(nameAndValue[0].trim()):nameAndValue[1].trim()]
+	}
+	log.debug "Desc Map: $descMap"
+ 
+	Map resultMap = [:]
+    if (descMap.cluster == "0406" && descMap.attrId == "0000") {
+    	def value = descMap.value.endsWith("01") ? "active" : "inactive"
+    	resultMap = getMotionResult(value)
+    } 
+ 
+	return resultMap
+}
+
+private Map getMotionResult(value) {
+	log.debug 'motion'
+	String linkText = getLinkText(device)
+	String descriptionText = value == 'active' ? "${linkText} detected motion" : "${linkText} motion has stopped"
+	return [
+		name: 'motion',
+		value: value,
+		descriptionText: descriptionText
+	]
+}
+
+def updated() {
+    response(configure())
 }
 
 def refresh()
 {
-	log.debug "JDE: Refresh called"
-	"st rattr 0x${device.deviceNetworkId} 1 0x406 0"
+	log.debug "JDE: Refresh called y"
+	//"st rattr 0x${device.deviceNetworkId} 1 0x406 0"
 }
 
 def configure() {
 	log.debug "JDE: Configuring bindings."
 	String zigbeeId = swapEndianHex(device.hub.zigbeeId)
 	def configCmds = [
-         	"zdo bind 0x${device.deviceNetworkId} 1 1 0x406 {${device.zigbeeId}} {}"
+     	"zdo bind 0x${device.deviceNetworkId} 1 1 0x406 {${device.zigbeeId}} {}"
 	]
-    return configCmds + refresh() // send refresh cmds as part of config
+    return configCmds + refresh()  // send refresh cmds as part of config
 }
 
 private hex(value) {
@@ -75,3 +123,4 @@ private byte[] reverseArray(byte[] array) {
     }
     return array
 }
+
